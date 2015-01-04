71e6de9917abe6f39e5314f5e34c396e7e0ff740
Cory Taylor <cory.c.taylor@gmail.com>
2015-01-04 16:51:03 -0500

    adding front-end components (gulp, bower, grunt, etc)

--- src/UI/LearningLoop.Web/Scripts/_references.js
+++ src/UI/LearningLoop.Web/Scripts/_references.js
@@ -1,2 +1,11 @@
-﻿/// <autosync enabled="true" />
-/// <reference path="../content/js/jsonviewer.js" />
+﻿//Add JS/CSS references here to enable intellisense from external packages.
+/// <autosync enabled="true" />
+/// <reference path="../gruntfile.js" />
+/// <reference path="../uitests/preprocessor.js" />
+/// <reference path="../uitests/unit/hello-test.js" />
+/// <reference path="../js/jsonviewer.js" />
+/// <reference path="~/bower_components/jquery/dist/jquery.js" />
+/// <reference path="~/bower_components/bootstrap/dist/js/bootstrap.js" />
+/// <reference path="~/node_modules/gulp/index.js"/>
+/// <reference path="~/bower_components/bootstrap/dist/css/bootstrap.css"/>
+/// <reference path="~/node_modules/grunt/lib/util/task.js"/>
